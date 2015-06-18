478593e7da491ec301261792740b7c940c2eddc3
wirawan-syscli <wirawan@syscli.co.jp>
2015-06-18 18:03:35 +0900

    woof 0.1

---
+++ woof/Annotation.swift
@@ -0,0 +1,13 @@
+//
+//  Annotation.swift
+//  woof
+//
+//  Created by wirawan sanusi on 6/18/15.
+//  Copyright (c) 2015 wirawan sanusi. All rights reserved.
+//
+
+import UIKit
+
+class Annotation: NSObject {
+   
+}
---
+++ woof/Location.swift
@@ -0,0 +1,9 @@
+//
+//  Location.swift
+//  woof
+//
+//  Created by wirawan sanusi on 6/18/15.
+//  Copyright (c) 2015 wirawan sanusi. All rights reserved.
+//
+
+import Foundation
